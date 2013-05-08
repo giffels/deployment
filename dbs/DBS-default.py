1dcaa7e258c1dba424a1c17b3db9aacaec169e6a
Manuel Giffels <giffels@gmail.com>
2013-05-08 11:06:45 +0200

    Add DBS Frontpage needed for RESTv2

--- dbs/DBS-default.py
+++ dbs/DBS-default.py
@@ -9,11 +9,13 @@ DBSVERSION = os.getenv('DBS3_VERSION')
 
 config = Configuration()
 main = config.section_('main')
+srv = main.section_('server')
+srv.thread_pool = 10
 main.application = 'dbs'
 main.port = 8250
-main.index = 'data'
+main.index = 'ui'
 
-main.authz_defaults = { 'role':None, 'group' : None, 'site' : None}
+main.authz_defaults = { 'role' : None, 'group' : None, 'site' : None}
 security = main.section_('tools').section_("cms_auth")
 security.key_file = os.path.join(ROOTDIR,'auth/wmcore-auth/header-auth-key')
 
@@ -23,6 +25,8 @@ app.description = 'Data Bookkeeping Service 3'
 app.title = 'CMS Data Bookkeeping Service'
 
 views = config.section_('views')
+ui = views.section_('ui')
+ui.object = 'dbs.web.DBSFrontPage.DBSFrontPage'
 
 data = views.section_('data')
 data.object = 'dbs.web.DBSRestApi.DBSRestApi'
