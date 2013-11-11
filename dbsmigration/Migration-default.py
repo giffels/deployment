2c9df4320fb57e2d86d3a6de395385f35f978ded
Manuel Giffels <giffels@gmail.com>
2013-11-11 17:37:33 +0100

    Use two thread for migration

--- dbsmigration/Migration-default.py
+++ dbsmigration/Migration-default.py
@@ -18,8 +18,8 @@ from DBSSecrets import dbs3_l_i2
 db_mapping = {'dev/global': dbs3_p1_i2,
               'dev/phys03': dbs3_l_i2}
 
-thread_mapping = {'dev/global': 1,
-                  'dev/phys03': 1}
+thread_mapping = {'dev/global': 2,
+                  'dev/phys03': 0}
 
 def create_instance_config(db_instances, instance_name):
     db_config_section = db_instances.section_(instance_name)
